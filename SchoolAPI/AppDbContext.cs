7970c4616eead362a9c3725b1c890b6caaacb643
Mohamed <moh.mus2019@gmail.com>
2026-05-15 16:22:46 +0300

    Connect Ef core to API

---
+++ SchoolAPI/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SchoolAPI.models;
+
+
+namespace SchoolAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+            public DbSet<Student> Students { get; set; }
+
+
+    }
+}
