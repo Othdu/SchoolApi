430774154a88b1f70503d53afa20b52b167d2d8c
Mohamed <moh.mus2019@gmail.com>
2026-05-15 19:46:38 +0300

    Added Dtos - seprate input and output models

---
+++ SchoolAPI/DTOs/StudentDto.cs
@@ -0,0 +1,15 @@
+﻿namespace SchoolAPI.DTOs
+{
+    public class StudentDto
+    {
+        public string? Name { get; set; }
+        public double Grade { get; set; }
+    }
+    public class StudentResponseDto
+    {
+        public int id {  get; set; }
+        public String? Name { get; set; }
+        public double Grade { get; set; }
+    }
+
+}
