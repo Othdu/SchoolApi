ca75d89ab6b3ae2921a0b58fcff0f24884173225
Mohamed <moh.mus2019@gmail.com>
2026-05-24 01:09:54 +0300

    JWT securety

---
+++ SchoolAPI/DTOs/AuthDto.cs
@@ -0,0 +1,28 @@
+﻿namespace SchoolAPI.DTOs
+{
+    public class RegisterDto
+    {
+
+        public string ? Email {  get; set; }
+        public string? Password { get; set; }
+ 
+    }
+    public class LoginDto
+    {
+
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+
+    }
+    public class AuthResponseDto
+    {
+
+        public string? Token { get; set; }
+        public string? Email { get; set; }
+        public string? Role { get; set; }
+
+
+    }
+
+
+}
